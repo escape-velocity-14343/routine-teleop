abe1657249effca444398cd53078b5926a1984a5
Fish <me@fishies.dev>
2025-09-06 16:00:38 -0700

    why doesn't it work

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,21 @@
 plugins {
-	id("dev.frozenmilk.teamcode") version "10.1.1-0.1.3"
+    kotlin("kapt") version "2.1.0"
+    id("dev.frozenmilk.teamcode") version "10.1.1-0.1.3"
 }
 
 ftc {
-	// use this to easily add more FTC libraries
+    kotlin
+}
 
-	// adds support for kotlin
-	kotlin
+repositories {
+    maven("https://maven.brott.dev/")
+}
+
+dependencies {
+    implementation("com.acmerobotics.dashboard:dashboard:0.4.17") {
+        exclude("org.firstinspires.ftc")
+    }
+    implementation("org.fishnpotatoes.routine:core")
+    implementation("org.fishnpotatoes.routine:util")
+    implementation("org.fishnpotatoes.routine:ftc")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,11 @@ pluginManagement {
 		maven("https://repo.dairy.foundation/releases")
 	}
 }
+
+includeBuild("../routine") {
+	dependencySubstitution {
+		substitute(module("org.fishnpotatoes.routine:core")).using(project(":core"))
+		substitute(module("org.fishnpotatoes.routine:util")).using(project(":util"))
+		substitute(module("org.fishnpotatoes.routine:ftc")).using(project(":ftc"))
+	}
+}
